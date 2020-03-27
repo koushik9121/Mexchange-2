13c4959c9a2a425b7a85589e7ab484e034d4a191
phitesh <phiteshreddy7@gmail.com>
2020-03-27 13:41:35 +0530

    commit

--- app/src/main/java/com/example/myapplication/ContactActivity.java
+++ app/src/main/java/com/example/myapplication/ContactActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ContactActivity extends AppCompatActivity {
 
     TextView mobile;
     TextView name;
     TextView userid;
+    Button request;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +27,17 @@ public class ContactActivity extends AppCompatActivity {
         String mobile_txt=intent.getExtras().getString("mobile");
         String name_txt=intent.getExtras().getString("name");
         String userid_txt=intent.getExtras().getString("userid");
-        name.setText("Offered by "+name_txt);
-        userid.setText("Roll No: "+userid_txt);
-        mobile.setText("Contact +91-"+mobile_txt+" for further information");
+        name.setText(name_txt);
+        userid.setText(userid_txt);
+        mobile.setText("+91-"+mobile_txt);
+        request = (Button)findViewById(R.id.request);
+        request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ContactActivity.this,"Request Sent!",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(ContactActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
